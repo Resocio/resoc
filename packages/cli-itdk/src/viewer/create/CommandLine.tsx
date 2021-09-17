25daf913891be9205dd64e2ada902f98c6301dbb
Philippe Bernard <philippe.bernard@gmail.com>
2021-09-17 10:56:00 +0200

    Simplify command line

--- packages/cli-itdk/src/viewer/create/CommandLine.tsx
+++ packages/cli-itdk/src/viewer/create/CommandLine.tsx
@@ -8,88 +8,22 @@ import CodeBlock from './CodeBlock';
 
 export type CommandLineProps = CreateImageProps;
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 16px;
-`;
-
-const CommandLineContainer = styled(Form)`
-  flex: 1;
-`;
-
-const CommandLineBox = styled(Alert)`
-  display: flex;
-  align-items: flex-start;
-`;
-
-const CmdLine = styled.pre`
-  white-space: pre-wrap;
-  flex: 1;
-`;
-
-const CopyButton = styled(Button)`
-`;
-
 export const paramValuesToCommandLine = (parameters: TemplateParam[], values: ParamValues): string => (
   parameters.map(p => `${p.name}="${paramValueToString(p, values[p.name]).replaceAll('"', '\\"')}"`).join(' ')
 );
 
 const CommandLine = (props: CommandLineProps) => {
-  const [ imageExt, setImageExt ] = useState<'jpg' | 'png'>('jpg');
-  const [ platform, setPlatform ] = useState<'facebook' | 'twitter'>('facebook');
+  const outputFile = 'output-image.jpg';
+  const dims = FacebookOpenGraph;
 
-  const outputFile = `output-image.${imageExt}`;
-  const dims = platform === 'facebook' ? FacebookOpenGraph : TwitterCard;
   const commandLine =
     `npx create-img ${props.manifestPath} -o ${outputFile} --params ${paramValuesToCommandLine(props.parameters, props.values)} -w ${dims.width} -h ${dims.height}`;
 
   return (
-    <Wrapper>
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="format">
-            Output format
-          </Form.Label>
-          <Form.Select
-            name="format"
-            value={imageExt}
-            onChange={(e) => {
-              // Although e.target is not a FormControlElement, value field *does* exist
-              const target: any = e.target;
-              setImageExt(target.value);
-            }}
-          >
-            <option value="jpg">JPG</option>
-            <option value="png">PNG</option>
-          </Form.Select>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label htmlFor="platform">
-            Platform
-          </Form.Label>
-          <Form.Select
-            name="platform"
-            value={platform}
-            onChange={(e) => {
-              const target: any = e.target;
-              setPlatform(target.value);
-            }}
-          >
-            <option value="facebook">Facebook Open Graph</option>
-            <option value="png">Twitter Card</option>
-          </Form.Select>
-        </Form.Group>
-      </Form>
-
-      <CommandLineContainer>
-        <Form.Label>
-          Command line
-        </Form.Label>
-        <CodeBlock commandLine code={commandLine} />
-      </CommandLineContainer>
-    </Wrapper>
+    <div>
+      <p>Open a shell and run:</p>
+      <CodeBlock commandLine code={commandLine} />
+    </div>
   );
 };
 
